1988f10dab45919973aa6176c7b630dd1db75068
PavanShadow <shadowfax9592@gmail.com>
2020-03-25 10:47:34 +0530

    hotel view add

--- app/models/hotel.model.js
+++ app/models/hotel.model.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 var Hotel = mongoose.model('Hotel', {
     name: {type: String},
     address: {type: String},
-    price: {type: String},
+    price: {type: Number},
     imageUrl: {type: String},
     timestamps: {type: Date, default: Date.now}
 

dad703d6dc3a74625f43b3ef6739c9506587a107
PavanShadow <shadowfax9592@gmail.com>
2020-03-25 08:46:04 +0530

    index add

--- index.js
+++ index.js
@@ -47,6 +47,8 @@ app.use((req, res, next) => {
 require('./app/routes/destination.routes.js')(app);
 
 // listen for requests
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
     console.log("Server is listening on port 3000");
 });
